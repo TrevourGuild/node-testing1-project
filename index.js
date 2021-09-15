477daad892c5caac745f979d7b0e45b39b17f274
TrevourGuild <trevorr.guildd@gmail.com>
2021-09-15 14:12:43 -0700

    8 tests passing

--- index.js
+++ index.js
@@ -7,8 +7,11 @@
  * trimProperties({ name: '  jane  ' }) // returns a new object { name: 'jane' }
  */
 function trimProperties(obj) {
-  // ✨ implement
-  
+  let result = {}
+  for( let prop in obj) {
+    result[prop] = obj[prop].trim()
+  }  
+  return result
 }
 
 /**
@@ -20,7 +23,10 @@ function trimProperties(obj) {
  * trimPropertiesMutation({ name: '  jane  ' }) // returns the object mutated in place { name: 'jane' }
  */
 function trimPropertiesMutation(obj) {
-  // ✨ implement
+  for( let prop in obj) {
+    obj[prop] = obj[prop].trim()
+  }  
+  return obj
 }
 
 /**
@@ -32,7 +38,13 @@ function trimPropertiesMutation(obj) {
  * findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]) // returns 3
  */
 function findLargestInteger(integers) {
-  // ✨ implement
+  let result = integers[0].integer
+  for(let i = 0; i < integers.length; i++){
+    if(integers[i].integer > result) {
+      result = integers[i].integer
+    }
+  }
+  return result 
 }
 
 class Counter {
@@ -42,6 +54,7 @@ class Counter {
    */
   constructor(initialNumber) {
     // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -57,7 +70,10 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    if(this.count > 0) {
+      return this.count--
+    }
+    return this.count
   }
 }
 
